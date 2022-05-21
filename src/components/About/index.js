8c38348ab3fae64b358f7dd67583df3e08dcde17
bryce <blarkin13@gmail.com>
2022-05-20 19:31:27 -0500

    about component

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className='my-5'>
+            <h1 id='about'>Bio</h1>
+            <p>My name is Bryce Larkin</p>
+        </section>
+    );
+}
+
+export default About;
